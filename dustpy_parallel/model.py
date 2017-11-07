26ee30a114168a1d2ec65daf41d8912115a9a7dc
Til Birnstiel <birnstiel@me.com>
2017-11-07 16:51:46 +0100

    reduced number of cores in parallel run to 12

--- dustpy_parallel/model.py
+++ dustpy_parallel/model.py
@@ -2,8 +2,8 @@ from dustpy import sim
 
 dustpy = sim.Simulation()
 
-# Using multithreading with 28 threads
-dustpy.pars.Nthreads = 28
+# Using multithreading with 12 threads
+dustpy.pars.Nthreads = 12
 dustpy.pars.verbose = 2
 
 # Initializing the model with standard parameters
