d2a7023b66d44f246d0293dd3f41ad19b98d4c7c
Til Birnstiel <birnstiel@me.com>
2017-11-09 16:56:47 +0100

    removed the setting of threads in the model which is now obsolete

--- dustpy_parallel/model.py
+++ dustpy_parallel/model.py
@@ -2,8 +2,6 @@ from dustpy import sim
 
 dustpy = sim.Simulation()
 
-# Using multithreading with 12 threads
-dustpy.pars.Nthreads = 12
 dustpy.pars.verbose = 2
 
 # Initializing the model with standard parameters
--- dustpy_serial/model.py
+++ dustpy_serial/model.py
@@ -2,8 +2,6 @@ from dustpy import sim
 
 dustpy = sim.Simulation()
 
-# Using multithreading with 1 threads
-dustpy.pars.Nthreads = 1
 dustpy.pars.verbose = 2
 
 # Initializing the model with standard parameters
